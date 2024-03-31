dc4430a8a6726bacde30c6dd5477cf54cff0565e
SergWebAcceptance <serg.web@acceptance.ltd>
2024-03-31 16:40:15 +0300

    fixed manager permission

--- src/app/emails/[slug]/page.jsx
+++ src/app/emails/[slug]/page.jsx
@@ -1,12 +1,14 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Link from "next/link";
+import { useSession } from "next-auth/react";
 import AddEmailForm from "@/components/AddEmailForm";
 
 function SingleProject({ params: { slug } }) {
   const [email, setEmail] = useState();
   const [editableForm, setEditableForm] = useState(false);
+  const { data: session, status } = useSession();
+  const userRole = session ? session.user.role : "";
   useEffect(() => {
     const fetchEmail = async () => {
       try {
@@ -31,21 +33,22 @@ function SingleProject({ params: { slug } }) {
       {email ? (
         <>
           <AddEmailForm projectData={email} editable={editableForm} />
-          {!editableForm ? (
-            <button
-              onClick={() => handleEditable()}
-              className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
-            >
-              Edit
-            </button>
-          ) : (
-            <button
-              onClick={() => handleEditable()}
-              className="mt-4 inline-block w-full rounded-lg bg-red-600 px-5 py-3 font-medium text-white sm:w-auto"
-            >
-              Cancel edit
-            </button>
-          )}
+          {userRole !== "manager" &&
+            (!editableForm ? (
+              <button
+                onClick={() => handleEditable()}
+                className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+              >
+                Edit
+              </button>
+            ) : (
+              <button
+                onClick={() => handleEditable()}
+                className="mt-4 inline-block w-full rounded-lg bg-red-600 px-5 py-3 font-medium text-white sm:w-auto"
+              >
+                Cancel edit
+              </button>
+            ))}
         </>
       ) : (
         "Loading"
--- src/app/projects/[slug]/page.jsx
+++ src/app/projects/[slug]/page.jsx
@@ -1,12 +1,15 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Link from "next/link";
 import ProjectForm from "@/components/ProjectForm";
+import { useSession } from "next-auth/react";
 
 function SingleProject({ params: { slug } }) {
   const [project, setProject] = useState();
   const [editableForm, setEditableForm] = useState(false);
+  const { data: session, status } = useSession();
+  const userRole = session ? session.user.role : "";
+
   useEffect(() => {
     const fetchProject = async () => {
       try {
@@ -14,7 +17,6 @@ function SingleProject({ params: { slug } }) {
         setProject(response.data.project);
       } catch (error) {
         console.error("Could not fetch projects", error);
-        // Тут можна додати обробку помилок
       }
     };
 
@@ -30,21 +32,22 @@ function SingleProject({ params: { slug } }) {
       {project ? (
         <>
           <ProjectForm projectData={project} editable={editableForm} />
-          {!editableForm ? (
-            <button
-              onClick={() => handleEditable()}
-              className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
-            >
-              Edit
-            </button>
-          ) : (
-            <button
-              onClick={() => handleEditable()}
-              className="inline-block w-full rounded-lg bg-red-600 px-5 py-3 font-medium text-white sm:w-auto"
-            >
-              Cancel edit
-            </button>
-          )}
+          {userRole !== "manager" &&
+            (!editableForm ? (
+              <button
+                onClick={() => handleEditable()}
+                className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+              >
+                Edit
+              </button>
+            ) : (
+              <button
+                onClick={() => handleEditable()}
+                className="inline-block w-full rounded-lg bg-red-600 px-5 py-3 font-medium text-white sm:w-auto"
+              >
+                Cancel edit
+              </button>
+            ))}
         </>
       ) : (
         "Loading"
